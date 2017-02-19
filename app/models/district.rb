d7dc763847a5612ec71e2d7bbd7be445e4d4e911
Aditya Sridhar <aditya@getvenga.com>
2017-02-18 23:18:42 -0500

    Made sure that district model will only have one state-district combination.

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,5 +1,6 @@
 class District < ActiveRecord::Base
   belongs_to :state
+  validates :name, uniqueness: {scope: :state, message: "should be only one district state combination"}
   def get_districts_by_state(state)
     return District.where(state: state)
   end
