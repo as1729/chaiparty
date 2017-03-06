08df8f0d2923ff18ea4d42fd413f15fa8122d5f1
Aditya Sridhar <aditya@getvenga.com>
2017-03-05 21:13:17 -0500

    Testing the ability for user authentication and receiving sign-up confirmation email after they have signedup on the website.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   # TODO: Figure out how we are going to reference district/state to the user. Can the user subscribe to reports of multiple district members? What if they don't specify a district?
 end
